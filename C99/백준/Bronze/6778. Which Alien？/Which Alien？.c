ac5219f15e2da9f0d12901964f7d3eb35c3b1f3d
MINSEO KIM <flower6713@naver.com>
2025-10-07 11:39:58 +0900

    [Bronze IV] Title: Which Alien?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/6778. Which Alien？/Which Alien？.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int antenna,eyes;
+    scanf("%d",&antenna);
+    scanf("%d",&eyes);
+    if (antenna >= 3 && eyes <=4) {
+        printf("TroyMartian\n");
+    }
+    if (antenna <= 6 && eyes >=2) {
+        printf("VladSaturnian\n");
+    }
+    if (antenna <= 2 && eyes <=3) {
+        printf("GraemeMercurian\n");
+    }
+    
+    return 0;
+}
