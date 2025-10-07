b20a862b0099f5e42ee26a561ddda70239c9b3c0
MINSEO KIM <flower6713@naver.com>
2025-10-07 11:28:47 +0900

    Update 카드 게임.c

--- C99/백준/Bronze/5522. 카드 게임/카드 게임.c
+++ C99/백준/Bronze/5522. 카드 게임/카드 게임.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+// int a, sum; 전역변수 선언시 0으로 자동초기화되지만 
 int main() {
-    int sum = 0;
+    int sum = 0; // 지역변수는 0으로 직접 초기화 필요
     for (int i = 0; i < 5; i++) {
         int a = 0;
         scanf("%d",&a);
@@ -9,4 +9,5 @@ int main() {
     }
     printf("%d",sum);
     return 0;
+
 }
